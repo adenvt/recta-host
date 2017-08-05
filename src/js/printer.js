abd3e492cacfc78c8aa01d287f0997d3e5cbe9f5
adenvt <adenvt@gmail.com>
2017-08-05 21:58:08 +0700

    fix linter

--- src/js/printer.js
+++ src/js/printer.js
@@ -5,45 +5,45 @@ import Serial from './adapter/serial.js'
 import Network from './adapter/network.js'
 
 const PRINTER_ADAPTER = {
-    USB: Usb,
-    SERIAL: Serial,
-    NETWORK: Network
+  USB: Usb,
+  SERIAL: Serial,
+  NETWORK: Network
 }
 
 export default class Printer extends EventEmitter {
-    constructor(adapter, options) {
-        super()
+  constructor (adapter, options) {
+    super()
 
-        if (adapter instanceof Adapter) {
-        	this.adapter = adapter
-        } else {
-            this.adapter = new PRINTER_ADAPTER[adapter.toUpperCase()](options)
-        }
-
-        this.adapter.on('open', () => {
-            this.emit('open')
-        })
-        this.adapter.on('close', () => {
-            this.emit('close')
-        })
-        this.adapter.on('error', (e) => {
-            this.emit('error', e)
-        })
+    if (adapter instanceof Adapter) {
+      this.adapter = adapter
+    } else {
+      this.adapter = new PRINTER_ADAPTER[adapter.toUpperCase()](options)
     }
 
-    get isOpen() {
-        return this.adapter.isOpen
-    }
+    this.adapter.on('open', () => {
+      this.emit('open')
+    })
+    this.adapter.on('close', () => {
+      this.emit('close')
+    })
+    this.adapter.on('error', (e) => {
+      this.emit('error', e)
+    })
+  }
 
-    print(data) {
-        this.adapter.write(data)
-    }
+  get isOpen () {
+    return this.adapter.isOpen
+  }
 
-    open() {
-        return this.adapter.open()
-    }
+  print (data) {
+    this.adapter.write(data)
+  }
 
-    close() {
-        return this.adapter.close()
-    }
+  open () {
+    return this.adapter.open()
+  }
+
+  close () {
+    return this.adapter.close()
+  }
 }
--- src/js/service.js
+++ src/js/service.js
@@ -5,86 +5,86 @@ import Printer from './printer.js'
 import { CFG } from './constant.js'
 
 export default class Service extends EventEmitter {
-    constructor() {
-        super()
+  constructor () {
+    super()
 
-        this.http = HTTP.createServer((req, res) => {
-            res.end('Recta Print Service');
+    this.http = HTTP.createServer((req, res) => {
+      res.end('Recta Print Service')
+    })
+    this.http.setTimeout(1000)
+
+    this.io = Socket(this.http)
+    this.io.use((socket, next) => {
+      const token = socket.handshake.query.token
+      const appkey = CFG.get('app.key')
+
+      if (token !== appkey) {
+        return next(new Error('Not Authorized'))
+      }
+
+      return next()
+    })
+    this.io.on('connection', (socket) => {
+      socket.on('message', (data) => {
+        this.emit('message', data)
+
+        this.printer.print(Buffer.from(data))
+      })
+    })
+
+    this.printer = new Printer(CFG.get('printer.adapter'), CFG.get('printer.option'))
+    this.printer.on('open', () => {
+      this.emit('printer:open')
+    })
+    this.printer.on('close', () => {
+      if (this.http.listening) {
+        this.io.close(() => {
+          this.http.close(() => {
+            this.emit('error', new Error('Printer has closed'))
+            this.emit('close')
+          })
         })
-        this.http.setTimeout(1000);
+      }
 
-        this.io = Socket(this.http)
-        this.io.use((socket, next) => {
-            const token = socket.handshake.query.token
-            const appkey = CFG.get('app.key')
+      this.emit('printer:close')
+    })
+    this.printer.on('error', (e) => {
+      this.emit('error', e)
+      this.emit('printer:error', e)
+    })
+  }
 
-            if (token !== appkey) {
-                return next(new Error('Not Authorized'))
-            }
+  start () {
+    return new Promise((resolve, reject) => {
+      this.http.listen(CFG.get('app.port'), () => {
+        this.printer.open().then(() => {
+          this.emit('open')
 
-            return next()
+          return resolve()
+        }).catch((e) => {
+          this.http.close(() => {
+            return reject(e)
+          })
         })
-        this.io.on('connection', (socket) => {
-            socket.on('message', (data) => {
-                this.emit('message', data)
+      }).once('error', (error) => {
+        return reject(error)
+      })
+    })
+  }
 
-                this.printer.print(new Buffer(data))
-            })
+  stop () {
+    return new Promise((resolve, reject) => {
+      this.io.close(() => {
+        this.http.close(() => {
+          this.printer.close().then(() => {
+            this.emit('close')
+
+            return resolve()
+          }).catch((e) => {
+            return reject(e)
+          })
         })
-
-        this.printer = new Printer(CFG.get('printer.adapter'), CFG.get('printer.option'))
-        this.printer.on('open', () => {
-            this.emit('printer:open')
-        })
-        this.printer.on('close', () => {
-            if (this.http.listening) {
-                this.io.close(() => {
-                    this.http.close(() => {
-                        this.emit('error', new Error('Printer has closed'))
-                        this.emit('close')
-                    })
-                })
-            }
-
-            this.emit('printer:close')
-        })
-        this.printer.on('error', (e) => {
-            this.emit('error', e)
-            this.emit('printer:error', e)
-        })
-    }
-
-    start() {
-        return new Promise((resolve, reject) => {
-            this.http.listen(CFG.get('app.port'), (err) => {
-                this.printer.open().then(() => {
-                    this.emit('open')
-
-                    return resolve()
-                }).catch((e) => {
-                    this.http.close(() => {
-                        return reject(e)
-                    })
-                })
-            }).once('error', (error) => {
-                return reject(error)
-            })
-        })
-    }
-
-    stop() {
-        return new Promise((resolve, reject) => {
-            this.io.close(() => {
-                this.http.close(() => {
-                    this.printer.close().then(() => {
-                        this.emit('close')
-
-                        return resolve()
-                    }).catch((e) => {
-                        return reject(e)
-                    })
-                })
-            })
-        })
-    }
+      })
+    })
+  }
 }
